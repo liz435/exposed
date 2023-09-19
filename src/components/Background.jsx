e06bc72600edb03ed181b44525c7551cfb58be1a
Zelong Li <87303807+liz435@users.noreply.github.com>
2023-09-18 21:10:24 -0400

    broken

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,27 +1,24 @@
-import React, { useState, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import Torus from "./Shapes/Torus";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 export default function Background(props) {
   const [speed, setSpeed] = useState(1);
-  const [shape, setShape] = useState("torus ");
+  const [shape, setShape] = useState("torus");
 
   const handleClick = (e) => {
-
-    if(e.target.value === 'speed up') {
+    if (e.target.value === "speed up") {
       setSpeed(speed * 2);
-      return
+    } else if (e.target.value === "slow down") {
+      setSpeed(speed / 2);
     }
-
-    setSpeed(speed / 2);
   };
 
   function handleChangeShape(e) {
-    
     switch (e.target.value) {
       case "sphere":
         setShape("sphere");
-        
         break;
       case "rect":
         setShape("rect");
@@ -29,30 +26,43 @@ export default function Background(props) {
       case "torus":
         setShape("torus");
         break;
-      }
+      case "cone":
+        setShape("cone");
+        break;
+      default:
+        break;
+    }
   }
 
   return (
-    <>
-      <button onClick={handleClick} value={'speed up'}>speed up</button>
-      <button onClick={handleClick} value={'slow down'}>slow down</button>
-      <button onClick={handleChangeShape} value={"sphere"}>
-        sphere
-      </button>
-      <button onClick={handleChangeShape} value={"rect"}>
-        rect
-      </button>
-      <button onClick={handleChangeShape} value={"torus"}>
-        torus
-      </button>
-      <button onClick={handleChangeShape} value={"cone"}>
-        cone
-      </button>
+    <Routes>
+    <Router>
+      <div>
+        <button onClick={handleClick} value="speed up">Speed Up</button>
+        <button onClick={handleClick} value="slow down">Slow Down</button>
+        <Link to="/sphere">
+          <button onClick={() => setShape("sphere")}>Sphere</button>
+        </Link>
+        <Link to="/rect">
+          <button onClick={() => setShape("rect")}>Rect</button>
+        </Link>
+        <Link to="/torus">
+          <button onClick={() => setShape("torus")}>Torus</button>
+        </Link>
+        <Link to="/cone">
+          <button onClick={() => setShape("cone")}>Cone</button>
+        </Link>
+      </div>
+
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Torus speed={speed} shape={shape}/>
+        <Route path="/sphere" render={() => <Torus speed={speed} shape="sphere" />} />
+        <Route path="/rect" render={() => <Torus speed={speed} shape="rect" />} />
+        <Route path="/torus" render={() => <Torus speed={speed} shape="torus" />} />
+        <Route path="/cone" render={() => <Torus speed={speed} shape="cone" />} />
       </Canvas>
-    </>
+    </Router>
+    </Routes>
   );
 }
